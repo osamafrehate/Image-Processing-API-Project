36ce637136a4fecf7cf1b6f6e5a8217421ac2ba7
osamafrehate <osama.frehate@gmail.com>
2025-09-05 20:08:10 +0300

    Delete all files

--- src/tests/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest'
-import { app } from '../../../index' 
-
-describe('GET /api', () => {
-  it('should return the API endpoint successfully', async () => {
-    const response = await request(app).get('/api')
-    expect(response.status).toBe(200)
-  })
-})
